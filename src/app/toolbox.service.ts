7853816c9cd99934095f72d5d1379acb68af763a
jsrmalvarez <jsrmalvarez@gmail.com>
2024-06-05 13:16:03 +0200

    Avoid tool repetition

--- src/app/toolbox.service.ts
+++ src/app/toolbox.service.ts
@@ -13,7 +13,7 @@ export class ToolboxService {
 
     public async getToolbox(): Promise<Tool[]> {
         const projects = await this.projectService.getAllProjects();
-        const toolnames = Array.from(projects.map(project => project.tools).flat()).sort((a:string, b:string) => a.toLowerCase().localeCompare(b.toLowerCase()));
+        const toolnames = [... new Set(Array.from(projects.map(project => project.tools).flat()).sort((a:string, b:string) => a.toLowerCase().localeCompare(b.toLowerCase())))];
         const toolbox = toolnames.map(toolname => ({ name: toolname, description: null, icon: null }));
         return toolbox;
     }
